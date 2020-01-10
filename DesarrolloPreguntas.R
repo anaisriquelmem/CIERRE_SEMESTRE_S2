72d190acdcb6d3b3a6a73455f2bf7ca1780b2bd9
amriquelme <am.riquelme96@gmail.com>
2020-01-10 01:12:38 -0300

    no message

--- DesarrolloPreguntas.R
+++ DesarrolloPreguntas.R
@@ -1,3 +1,8 @@
-ijkj
-dd
-
+#Ejercicio 1
+ejemplos = sample(c("positivo", "negativo", "neutros"), 100, replace = TRUE)
+#positivos
+num_positivos<-0
+for(i in 1:length(ejemplos)){
+  if("positivo"==ejemplos[i]){num_positivos<-num_positivos+1}
+}
+#negativos
